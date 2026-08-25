2143afd25ff69b7959cf77d562141bc43aeacbfb
Tim Köhler <33375734+timksf@users.noreply.github.com>
2026-08-25 15:48:14 +0200

    update

--- libraries/BlueFabric/BlueFabric.mk
+++
@@ -1,7 +0,0 @@
-BLUEFABRIC_ROOT := $(abspath $(dir $(lastword $(MAKEFILE_LIST)))/../../dep/bluefabric)
-BLUEFABRIC_SRC := $(BLUEFABRIC_ROOT)/src
-BLUEFABRIC_SRC_DIRS := $(shell find $(BLUEFABRIC_SRC) -type d -print)
-
-EXTRA_BSV_LIBS += $(BLUEFABRIC_SRC_DIRS)
-
-$(info Adding BlueFabric from $(BLUEFABRIC_SRC))
